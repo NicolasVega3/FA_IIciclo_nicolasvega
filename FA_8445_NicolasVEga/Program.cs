c942b1ff7c13be9215e3acf02fd8124a8538adf1
nicolas vega <nicolasvegaquispe2018@gmail.com>
2025-09-05 08:56:22 -0500

    subiendo ejercicio 3

--- FA_8445_NicolasVEga/Program.cs
+++ FA_8445_NicolasVEga/Program.cs
@@ -10,7 +10,7 @@ namespace FA_8445_NicolasVEga
     {
         static void Main(string[] args)
         {
-            ejer2();
+            ejer3();
             Console.ReadKey();
         }
         static void ejer1()
@@ -29,6 +29,22 @@ namespace FA_8445_NicolasVEga
         static void ejer2()
         {
             Console.WriteLine("\"Nicolas\"");
+        }
+
+        static void ejer3()
+        {
+            Console.Write("Ingrese num 1: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese num 2: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            double divi = x / y;
+
+            Console.WriteLine("\nSuma: " + (x + y));
+            Console.WriteLine("Resta: " + (x - y));
+            Console.WriteLine("Multiplicación: " + (x * y));
+            Console.WriteLine("División: " + divi);
         }
     }
 }
